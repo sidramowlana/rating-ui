35494a0a0252e0c505eb3f1aeb0c62fc8ac0f921
sidramowlana <sidra.sm18@gmail.com>
2026-08-31 15:44:27 +0530

    Basic Rating ui Setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Rating from "./components/Rating";
+
+const App = () => {
+  const name = "Sidra";
+  return (<>
+  {/* <h1>Hello {name}</h1> */}
+   <Rating /> </> );
+}
+ 
+export default App;
---
+++ src/components/Rating.jsx
@@ -0,0 +1,21 @@
+const Rating = () => {
+  return (
+    <>
+      <div style={styles.container}>
+        <h1 style={styles.heading}>Rate Your Experience</h1>
+      </div>
+    </>
+  );
+};
+
+const styles = {
+  container: {
+    textAlign: "center",
+    fontFamily: "Arial, sans-serif",
+    padding: "20px",
+  },
+  heading:
+    {color: "#ee6c6c"}
+  
+};
+export default Rating;
